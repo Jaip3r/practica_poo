19726eaccd33d9df61ba391502b621c5e58fb54a
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2026-06-02 22:20:09 -0500

    Creando interfaz para calculo de intereses en cuentas de ahorro e inversión.

---
+++ src/main/java/com/poo/polimorfismo/practica_1/Rentable.java
@@ -0,0 +1,9 @@
+package com.poo.polimorfismo.practica_1;
+
+public interface Rentable {
+    /**
+     * Calcula los intereses generados por la cuenta.
+     * @return los intereses generados por la cuenta
+     */
+    double calcularIntereses();
+}
